50c59331847a23390e653b5856dea42d363852a0
Kirill <cyrillekonnov@yandex.ru>
2023-05-07 13:52:13 +0300

    create validators password and email

--- validators/registration_check.py
+++ validators/registration_check.py
@@ -1,25 +1,49 @@
 import re
+from abc import ABC, abstractmethod
 
 
-class CheckRegister:
-    def is_valid_email(self, email):
-        """Эта функция проверяет, соответствует ли переданный email заданному шаблону регулярного выражения.
-        Шаблон проверяет, что строка начинается с одной или более букв, цифр, знаков препинания,
-        а затем следует символ @, после которого идет доменное имя (опять же, из букв, цифр и знаков препинания),
-        и оканчивается на ., за которым следует две или более буквы."""
-        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        return re.match(pattern, email) is not None
-
-    def is_valid_password(self, password):
-        """Пароль должен содержать от 8 до 20 символов
-        Пароль должен содержать хотя бы одну цифру
-        Пароль должен содержать хотя бы одну заглавную букву
-        Пароль должен содержать хотя бы одну строчную букву
-        Пароль должен содержать хотя бы один символ пунктуации"""
-        # pattern = r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])(?!.*\s).{8,20}$'
-        # return re.match(pattern, password) is not None
-        return True if len(password) > 2 else False
+class CheckRegister(ABC):
+    @abstractmethod
+    def is_valid_lenght(self, lenght_password):
+        pass
 
 
-tmp = CheckRegister()
+class CheckUserPassword(CheckRegister):
+
+    def is_valid_lenght(self, lenght_password):
+        "Функция проверяет , что длина пароля валидная"
+        return True if len(lenght_password) >= 8 else False
+
+    def checking_that_the_password_contains_at_least_one_digit(self, password):
+        "Функция проверяет , что пароль содержит хотя бы одну цифру"
+        return any(psw.isdigit() for psw in password)
+
+    def has_uppercase(self, password):
+        """Проверяет, содержит ли пароль хотя бы одну заглавную букву"""
+        pattern = r'[A-Z]'
+        return bool(re.search(pattern, password))
+
+    def has_special_character(self, password):
+        """Проверяет, содержит ли пароль хотя бы один спец символ"""
+        regex = r"[ !#$%&'()*+,-./:;<=>?@[\]^_`{|}~]"
+        return bool(re.search(regex, password))
+
+
+class CheckUserEmail(CheckRegister):
+    def is_valid_lenght(self, lenght_email):
+        "Функция проверяет , что длина пароля валидная"
+        return True if len(lenght_email) >= 5 else False
+
+    def count_dog_symbol(self, email):
+        "Функция проверяет , что  email содержит символ @"
+        return True if email.count("@") == 1 else False
+
+    def count_dot_email(self, email):
+        "Функция проверяет , что  email содержит символ . "
+        return True if email.count('.') == 1 else False
+
+    def email_should_not_exceed_voltage(self, email):
+        "Функция проверяет , что длина email не более 256"
+        return True if len(email) <= 256 else False
+
 
