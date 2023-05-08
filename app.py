a9cd85e5605d09d142c44ce5d8ff0fcb36bb0526
Kirill <cyrillekonnov@yandex.ru>
2023-05-08 13:14:53 +0300

    DATABASE SECRET_KEY

--- app.py
+++ app.py
@@ -2,21 +2,22 @@ from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from validators.registration_check import CheckUserPassword, CheckUserEmail
-
+from config import DATABASE, SECRET_KEY
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/economical_site'
-app.secret_key = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.secret_key = SECRET_KEY
 validator_password = CheckUserPassword()
 validator_email = CheckUserEmail()
-
 db = SQLAlchemy(app)
+
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(256), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
